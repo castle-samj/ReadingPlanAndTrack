6ec4a174d7332c0f40628818c7c1e9c25691beb1
Sam Castle <castle.samj@gmail.com>
2024-06-10 11:59:02 -0400

    starting some timer component logic

---
+++ testingAlgorithms/python/timeMaster.py
@@ -0,0 +1,9 @@
+# General time tracking master class for functions
+
+# reference site: 
+# https://docs.python.org/3/library/datetime.html#examples-of-usage-datetime
+
+# generate data components on demand:
+#   - UTC timestamp of "start" and "stop" instances
+#   - (UTC/Localized) current date (for book start and end dates)
+#       - using the datetime.now(timezone.utc) creates a TZINFO value
